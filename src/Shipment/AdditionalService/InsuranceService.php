2cf900b1e5003bceb73085a8e3d95d43788b9350
Rimvydas V <lithstud@gmail.com>
2023-11-10 15:51:05 +0200

    InsureService params. Cant be convertedfrom legacy and must be handled manualy

--- src/Shipment/AdditionalService/InsuranceService.php
+++ src/Shipment/AdditionalService/InsuranceService.php
@@ -2,11 +2,21 @@
 
 namespace Mijora\Omniva\Shipment\AdditionalService;
 
+use Mijora\Omniva\OmnivaException;
+
 class InsuranceService implements AdditionalServiceInterface
 {
     const CODE = 'INSURANCE';
 
-    const PARAMS_LIST = [];
+    const PARAM_INSURANCE_VALUE = "INSURANCE_VALUE";
+
+    const PARAMS_LIST = [
+        self::PARAM_INSURANCE_VALUE,
+    ];
+
+    private $params = [
+        self::PARAM_INSURANCE_VALUE => null,
+    ];
 
     /**
      * {@inheritdoc}
@@ -21,6 +31,29 @@ class InsuranceService implements AdditionalServiceInterface
      */
     public function getServiceParams()
     {
-        return null;
+        return $this->params;
+    }
+
+    public function setParam($key, $value)
+    {
+        if (!in_array($key, self::PARAMS_LIST)) {
+            throw new OmnivaException("Invalid InsuranceService param key");
+        }
+
+        $this->params[$key] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Insurance value. The separator for the fraction is a decimal point.
+     * 
+     * @param float $amount
+     * 
+     * @return InsuranceService
+     */
+    public function setInsuranceValue($value)
+    {
+        return $this->setParam(self::PARAM_INSURANCE_VALUE, (float) $value);
     }
 }
